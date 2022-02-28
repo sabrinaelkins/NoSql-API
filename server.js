3a983fe52558b591c09b859b3072bca60f196a41
sabrinaelkins <sabrina.elkins12@gmail.com>
2022-02-28 13:05:03 -0600

    commit updates to controllers

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require("express");
-const mongodb = require("./config")
-const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose")
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+app.use(require("./routes"));
 
-
-// routes
-app.use(require("./routes/api.js"));
-mongodb.once("open", function () {
-    app.listen(PORT, () => {
-        console.log(`App running on port ${PORT}!`);
-    });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/social-app-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+    
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));

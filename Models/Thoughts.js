dcaf669df358268b12dad89d0b9b05120eb6befa
sabrinaelkins <sabrina.elkins12@gmail.com>
2022-02-28 13:34:11 -0600

    updates to models

--- Models/Thoughts.js
+++ Models/Thoughts.js
@@ -1,31 +1,36 @@
-const {Schema,model} = require("mongoose");
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
 
 const thoughtsSchema = new Schema({
   thoughtText: {
     type: String,
+    minLength: 1,
+    maxLength: 200,
     required: "Enter a thought!"
   },
   createdAt: {
     type: Date,
-    default: Date.now
-},
-reaction:[ 
-    {
-        type: Schema.Types.ObjectId,
-        ref: "Reactions"
-    }
-],
+    default: Date.now,
+    get: timeStamp => dateFormat(timeStamp)
+  },
+  reaction: [reactionSchema],
+
   username: {
     type: String,
     trim: true,
     required: true
   },
 }, {
-    toJSON: {
-        virtuals: true
-    },
-        id: false
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
 })
+
+
+
 thoughtsSchema.virtual("reactioncount").get(() => this.reaction.length)
 
 
